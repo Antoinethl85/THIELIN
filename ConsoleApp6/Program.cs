00c4293f98cbf4f7df69b1766f36ed3958c4759c
Antoinethl85 <antoine85thl@gmail.com>
2022-06-14 22:45:51 +0200

    init project

---
+++ ConsoleApp6/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Net.Http;
+
+namespace ConsoleApp6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var client = new HttpClient();
+            var request = new HttpRequestMessage();
+            
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
