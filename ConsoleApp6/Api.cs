ed9d3c9f0c87473ec489b8dd872eaf0c1aadb317
Antoinethl85 <antoine85thl@gmail.com>
2022-06-16 00:18:18 +0200

    Question 1

--- ConsoleApp6/Api.cs
+++ ConsoleApp6/Api.cs
@@ -4,6 +4,7 @@ using System.Net.Http;
 using System.Runtime.CompilerServices;
 using System.Threading.Tasks;
 using ConsoleApp6.API;
+using Newtonsoft.Json;
 
 
 namespace ConsoleApp6
@@ -11,21 +12,32 @@ namespace ConsoleApp6
     public class Api
     {
         private HttpClient _client;
-        private static string _url = "";
+        private static string _url = "https://api.openweathermap.org/data/3.0/onecall";
         private string _key;
 
+        //A MODIFIER
         public Api(Api _api)
         {
             _client = new HttpClient();
         }
 
-        /*public Root getWeatherMaroco(string lat, string lon)
+        public Root getWeatherMaroco(string lat, string lon)
         {
-            var json = 
-        }*/
+            Dictionary<string, string> content = new Dictionary<string, string>
+            {
+                { "lat", lat },
+                { "lon", lon }
+            };
+            
+            var json = Request(_client, content).GetAwaiter().GetResult();
+            Root data = JsonConvert.DeserializeObject<Root>(json);
+
+            return data;
+        }
 
         private async Task<string> Request(HttpClient client, Dictionary<string, string> parameters)
         {
+            //A MODIFIER (chaquer avec API Marketstack
             string url = "";
             var reponse = await client.GetAsync(url);
             if (reponse.IsSuccessStatusCode)
--- ConsoleApp6/Program.cs
+++ ConsoleApp6/Program.cs
@@ -7,12 +7,7 @@ namespace ConsoleApp6
     {
         static void Main(string[] args)
         {
-            var client = new HttpClient();
-            var requestMaroco = new HttpRequestMessage
-            {
-                Method = HttpMethod.Get,
-                RequestUri = new Uri("https://api.openweathermap.org/data/3.0/onecall?lat={lat}&lon={lon}&exclude={part}&appid=624c4ed18365308ce225acee2b5c2bd2"),
-            };
+            
             
         }
     }
