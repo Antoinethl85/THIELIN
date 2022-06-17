343b857d125e32f6f7a5fcd2d77134421190a148
Antoinethl85 <antoine85thl@gmail.com>
2022-06-17 19:13:53 +0200

    changes

--- ConsoleApp6/Api.cs
+++ ConsoleApp6/Api.cs
@@ -6,17 +6,17 @@ using System.Threading.Tasks;
 using ConsoleApp6.API;
 using Newtonsoft.Json;
 
-
 namespace ConsoleApp6
 {
     public class Api
     {
+        
         private HttpClient _client = new HttpClient();
-        private static string _url = "https://api.openweathermap.org/data/3.0/onecall";
+        private static string _url = "https://api.openweathermap.org/data/2.5/onecall";
         private string _key = "624c4ed18365308ce225acee2b5c2bd2";
         
 
-        public Root GetWeatherMorocco(string lat, string lon)
+        public Root GetWeatherMorocco(Coordonates coordinates)
         {
             Dictionary<string, string> content = new Dictionary<string, string>
             {
@@ -24,21 +24,22 @@ namespace ConsoleApp6
                 { "lon", lon }
             };
             
-            var json = Request(_client, content).GetAwaiter().GetResult();
+            var json = Request(_client, coordinates).GetAwaiter().GetResult();
             Root data = JsonConvert.DeserializeObject<Root>(json);
 
             return data;
         }
 
-        private async Task<string> Request(HttpClient client, Dictionary<string, string> parameters)
+        private async Task<string> Request(HttpClient client,Coordonates coordinates)
         {
-            string url = $"{_url}&appid={_key}";
+            string url = $"{_url}?lat={coordinates.lat}&lon={coordinates.lon}&dt={time}&appid={_key}";
             var reponse = await client.GetAsync(url);
-            
+
             if (reponse.IsSuccessStatusCode)
             {
                 return await reponse.Content.ReadAsStringAsync();
             }
+
             throw new Exception(await reponse.Content.ReadAsStringAsync());
         }
     }
---
+++ ConsoleApp6/Coordonates.cs
@@ -0,0 +1,14 @@
+﻿namespace ConsoleApp6
+{
+    public class Coordonates
+    {
+        public double lat { get; set; }
+        public double lon { get; set; }
+
+        public Coordonates(double _latitude, double _longitude)
+        {
+            _latitude = lat;
+            _longitude = lon;
+        }
+    }
+}
